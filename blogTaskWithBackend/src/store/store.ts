6c8361e4e614db2f37268fadfc1fe6056ae47536
shohan2032 <mohammad.shohan2032@gmail.com>
2025-02-21 18:38:48 +0600

    remove unused blogSlice and related imports

--- blogTaskWithBackend/src/store/store.ts
+++ blogTaskWithBackend/src/store/store.ts
@@ -1,12 +1,10 @@
 // store.js
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
-// import blogReducer from "../slices/blogSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    // favoriteBlogs: blogReducer,
   },
 });
 
